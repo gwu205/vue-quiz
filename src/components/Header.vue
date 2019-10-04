8f9ca0ad2547ef18b8beb539dfb28bea56bf15dd
Geoffrey Wu <gwu20510@gmail.com>
2019-10-04 14:19:37 +0900

    api call

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Header</h1>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><strong>Quiz App</strong></b-nav-item>
+            <b-nav-item disabled>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
 </template>
